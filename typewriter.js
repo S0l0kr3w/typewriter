e78d0b9f47100d1bf091cdc55ec9c31b7d3d10d4
Jason Junio <jjjunio@gmail.com>
2020-05-05 17:51:14 +0000

    created typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs";
+
+const printAnimation = function(sentence) {
+  for (const char in sentence) {
+    setTimeout(() => {
+      process.stdout.write(sentence[char]);
+    }, 100 * char)
+  }
+  
+};//end
+printAnimation(sentence);
+
+
+
+// for each character of the sentence 
+//print it out with a delay of x ms after the previous char in the sentence
+
